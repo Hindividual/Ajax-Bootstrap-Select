226a2fed78e47eee94e5fe360086d523f18b7e16
Hindividual <hkornbluth@renttherunway.com>
2019-10-18 16:37:31 -0400

    Adding Hebrew translation for ajaxSelectPicker

---
+++ src/js/ajaxSelectPicker.locale/he-IL.js
@@ -0,0 +1,71 @@
+/*!
+ * Hebrew translation for the "he-IL" and "he" language codes.
+ * Hindi Kornbluth <Hindi.Kornbluth@gmail.com>
+ */
+$.fn.ajaxSelectPicker.locale["he-IL"] = {
+
+    /**
+     * @member $.fn.ajaxSelectPicker.locale
+     * @cfg {String} currentlySelected = 'Currently Selected'
+     * @markdown
+     * The text to use for the label of the option group when currently selected options are preserved.
+     */
+    currentlySelected: 'נבחר כרגע',
+
+    /**
+     * @member $.fn.ajaxSelectPicker.locale
+     * @cfg {String} emptyTitle = 'Select and begin typing'
+     * @markdown
+     * The text to use as the title for the select element when there are no items to display.
+     */
+    emptyTitle: 'בחר והתחל להקליד',
+
+    /**
+     * @member $.fn.ajaxSelectPicker.locale
+     * @cfg {String} errorText = ''Unable to retrieve results'
+     * @markdown
+     * The text to use in the status container when a request returns with an error.
+     */
+    errorText: 'לא יכול להחזיר תוצאות',
+
+    /**
+     * @member $.fn.ajaxSelectPicker.locale
+     * @cfg {String} searchPlaceholder = 'Search...'
+     * @markdown
+     * The text to use for the search input placeholder attribute.
+     */
+    searchPlaceholder: '...חיפוש',
+
+    /**
+     * @member $.fn.ajaxSelectPicker.locale
+     * @cfg {String} statusInitialized = 'Start typing a search query'
+     * @markdown
+     * The text used in the status container when it is initialized.
+     */
+    statusInitialized: 'תתחיל להקליד שאילתה חיפוש',
+
+    /**
+     * @member $.fn.ajaxSelectPicker.locale
+     * @cfg {String} statusNoResults = 'No Results'
+     * @markdown
+     * The text used in the status container when the request returns no results.
+     */
+    statusNoResults: 'אין תוצאה',
+
+    /**
+     * @member $.fn.ajaxSelectPicker.locale
+     * @cfg {String} statusSearching = 'Searching...'
+     * @markdown
+     * The text to use in the status container when a request is being initiated.
+     */
+    statusSearching: '...חיפוש',
+
+    /**
+     * @member $.fn.ajaxSelectPicker.locale
+     * @cfg {String} statusTooShort = 'Please enter more characters'
+     * @markdown
+     * The text used in the status container when the request returns no results.
+     */
+    statusTooShort: ' להקליד יותר תווים'
+};
+$.fn.ajaxSelectPicker.locale.he = $.fn.ajaxSelectPicker.locale['he-IL'];
